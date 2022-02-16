8073b96b6a4f363a7a44d693cd104288e3a726b3
Carl Zou <carlzoulingxiang>
2022-02-16 15:25:16 -0800

    added helper func

---
+++ R/get_bearer.R
@@ -0,0 +1,13 @@
+get_bearer <- function() {
+    key <- Sys.getenv("key")
+    secret<-Sys.getenv("secret")
+    if (key == "" | secret == "") {
+        stop("You must provide an API key to the function or use `Sys.setenv(key = 'your_key_here')`, `Sys.setenv(secret = 'your_secret_key_here')`")
+    }
+    app_keys <- openssl::base64_encode(paste0(key, ":", secret))
+    r <- httr::POST("https://api.twitter.com/oauth2/token",
+                    httr::add_headers(Authorization = paste0("Basic ", app_keys)),
+                    body = list(grant_type = "client_credentials"))
+    bearer <- httr::content(r, encoding = "UTF-8")
+    bearer
+}
