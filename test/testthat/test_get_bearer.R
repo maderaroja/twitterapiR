df9554548f4f6778bcc8af9bff3d1d7da0abe225
Carl <46111943+carlzoulingxiang@users.noreply.github.com>
2022-02-18 15:20:34 -0800

    Update test_get_bearer.R

--- test/testthat/test_get_bearer.R
+++ test/testthat/test_get_bearer.R
@@ -2,12 +2,14 @@ library(testthat)
 library(twitterapiR)
 
 test_that("API key is retreived", {
+    skip_if_no_auth()
     Sys.setenv(key = Sys.getenv("TWITTER_API_KEY"))
     Sys.setenv(secret =  Sys.getenv("TWITTER_API_SECRET"))
     expect_type(get_bearer(), "list")
 })
 
 test_that("Invalid bearer key correctly fails", {
+    skip_if_no_auth()
     # invalid key and secret
     Sys.setenv(key = 'xxxxxxxxxxxxxxxxx')
     Sys.setenv(secret = 'xxxxxxxxxxxxxxxxxxx')
